6e8fc32406d3b791412f16c9f63bb306136efb20
Robert Northard <robertnorthard@googlemail.com>
2017-02-19 11:32:33 +0000

    Use single quotes for strings.

--- server/api/api.js
+++ server/api/api.js
@@ -2,10 +2,9 @@
 
 var api = {};
 
-api.properties = function () {
-  return {
-    'cartridges_url': process.env.CARTRIDGES_URL || 'https://gist.githubusercontent.com/kramos/ae04ccbb542ca7661b5568ae44c9f76f/raw/cartridges.yml'
-  };
+api.properties = function() {
+	return {
+		'cartridges_url': process.env.CARTRIDGES_URL || 'https://gist.githubusercontent.com/kramos/ae04ccbb542ca7661b5568ae44c9f76f/raw/cartridges.yml' };
 };
 
 module.exports = api;
